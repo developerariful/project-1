97c34b3ddb009287e5918a71aab8740d109d374e
Ariful <developerariful27@gmail.com>
2023-09-30 23:59:28 +0600

    slove

---
+++ project-1/src/component/slove.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const slove = () => {
+  return (
+    <div>
+      <p>hello slove </p>
+    </div>
+  );
+};
+
+export default slove;
--- src/Ariful/page.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const page = () => {
-  return <div>Ariful</div>;
-};
-
-export default page;
-Ariful;
